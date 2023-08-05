86d2497e504534fac757d5a21690036b14f56a12
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-08-05 19:05:14 +0530

    Update clients.py

--- TeamLegend/core/clients.py
+++ TeamLegend/core/clients.py
@@ -1,6 +1,23 @@
 from telegram.ext import Application
+from .client import 
+from pyrogram import Client
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 
 from .logger import LOGS
+loop = None
 
 LOGS.info("Client Starting")
 application = Application.builder().token("TOKEN").build()
+tbot = TelegramClient(
+    session="LegendTBot",
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=TOKEN)
+
+pbot = Client("LegendPBot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+
