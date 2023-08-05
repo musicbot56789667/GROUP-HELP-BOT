da6065b50dde4f50e427bb2464fef26d53c6f4ff
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-08-05 18:37:31 +0530

    Update clients.py

--- TeamLegend/core/clients.py
+++ TeamLegend/core/clients.py
@@ -5,18 +5,10 @@ import logging
 # enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    handlers=[logging.FileHandler("teamlegend.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
 
 LOGS = logging.getLogger(__name__)
-
-
-# if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error(
-        "You must have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
-    )
-    quit(1)
   
 application = Application.builder().token("TOKEN").build()
