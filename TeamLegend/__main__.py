e155b00f50c8e9cf8c1374a57ec96b789bc0030d
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-08-05 18:54:35 +0530

    Create __main__.py

---
+++ TeamLegend/__main__.py
@@ -0,0 +1,19 @@
+from .core.logger import LOGS
+from .core.clients import legendtl, legendpb, application
+from .Config import TOKEN
+from telegram import Update
+
+from TeamLegend.plugins import ALL_MODULES
+
+def main():
+    """Run bot."""
+    LOGS.info("Successfully loaded modules: " + str(ALL_MODULES))
+    legendtl.start(bot_token=TOKEN)
+    legendpb.start()
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+    
+if __name__ == "__main__":
+    main()
